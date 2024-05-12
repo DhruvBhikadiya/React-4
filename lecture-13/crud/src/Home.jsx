a7b51382d761fcad5dbafb24d3e5299897d6630b
Dhruv Bhikadiya <138368408+DhruvBhikadiya@users.noreply.github.com>
2024-05-12 19:28:42 +0530

    12-5-24

--- lecture-13/crud/src/Home.jsx
+++ lecture-13/crud/src/Home.jsx
@@ -1,4 +1,6 @@
 import { useEffect,useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Home = () => {
 
@@ -6,6 +8,7 @@ const Home = () => {
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
     const [user,setUSer] = useState([]);
+    const [edit, setEdit] = useState(null);
 
     // INSERT START
     const handleSubmit = (e) => {
@@ -19,7 +22,7 @@ const Home = () => {
             body: JSON.stringify({ userName, email, password }),
         })
         .then((data) => {
-            console.log(data);
+            console.log("submited data :- ",data);
 
             setUSerName("")
             setEmail("")
@@ -33,7 +36,7 @@ const Home = () => {
         fetch("http://localhost:8000/users")
         .then(res => res.json())
         .then((data) => {
-            console.log(data)
+            console.log("view data :- ",data)
             setUSer(data);
         })
         .catch((e)=>{
@@ -49,8 +52,8 @@ const Home = () => {
 
     // DELETE START
     const handleDelete = (id) => {
-        const deletedata = user.filter((item) => item.id !== id)
-        setUSer(deletedata);
+        const deleteData = user.filter((item) => item.id !== id)
+        setUSer(deleteData);
 
         fetch(`http://localhost:8000/users/${id}`,{
             method: "delete"
@@ -58,6 +61,36 @@ const Home = () => {
         .then((res) => console.log("data deleted successfully"));
     }
     // DELETE END
+
+    // UPDATE START
+    const handleEdit = (id) => {
+        const editData = user.find((item) => item.id == id)
+        // const editData = user.includes(id);
+        setEdit(editData);
+    }
+
+    const handleUpdate = () => {
+        fetch(`http://localhost:8000/users/${edit.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                userName: edit.userName,
+                email: edit.email,
+                password: edit.password
+            })
+        })
+        .then((res)=>res.json())
+        .then((data)=>{
+            console.log("Data updated",data);
+            toast.success("Data updated successfully");
+        })
+
+    }
+    // UPDATE END
+
+    console.log("edit :- ",edit)
 
     return (
         <div>
@@ -70,20 +103,34 @@ const Home = () => {
                 <button type="submit">Submit</button>
             </form>
             {/* INSERT END */}
-                
-            
 
-            {/* VIEW START  */}
-            {user.map((v,i) => (
-                <div key={v.id} style={{boxShadow:"1px 1px 10px 0px white",borderRadius:"70px",padding:"50px",marginTop:"50px"}}>
-                    <h1>{v.userName}</h1>
-                    <h2>{v.email}</h2>
-                    <h2>{v.password}</h2><br /><br />
-                    <button onClick={() => handleDelete(v.id)}>Delete</button>
-                </div>
-            ))}
-            {/* VIEW END */}
+            <br /><br />
 
+            <div>
+                {
+                    (edit && <div>
+                        <input type="text" value={edit.userName} onChange={(e) => setEdit({...edit,userName:e.target.value})} placeholder="Please enter username" /> <br /><br />
+                        <input type="text" value={edit.email} onChange={(e) => setEdit({...edit,email:e.target.value})} placeholder="Please enter email" /> <br /><br />
+                        <input type="text" value={edit.password} onChange={(e) => setEdit({...edit,password:e.target.value})} placeholder="Please enter password" /> <br /><br />
+                        <button onClick={handleUpdate}>Update</button>
+                    </div>)
+                }
+                {/* VIEW START  */}
+                {user.map((v,i) => (
+                    <div key={v.id} style={{boxShadow:"1px 1px 10px 0px white",borderRadius:"70px",padding:"50px",marginTop:"50px"}}>
+                        <h1>{v.userName}</h1>
+                        <h2>{v.email}</h2>
+                        <h2>{v.password}</h2><br /><br />
+                        <div style={{display:"flex", alignItems:"center",justifyContent:"space-between"}}>
+                            <button onClick={() => handleDelete(v.id)}>Delete</button>
+                            <button onClick={() => handleEdit(v.id)}>Update</button>
+                        </div>
+                    </div>
+                ))}
+                {/* VIEW END */}
+            </div>
+
+        <ToastContainer/>
         </div>
     )
 }
